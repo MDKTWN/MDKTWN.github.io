f2db38d1351ac3cbb700a87d2f2cead22a5e6549
TWN <137583009+MDKTWN@users.noreply.github.com>
2023-07-14 18:56:02 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ YT.ready(_=>{
             "onReady": e => {
                 e.target.setVolume(98);
                 clearInterval(loadInterval)
-                button.innerText = "Arceneaux $$$ ;)"
+                button.innerText = "Ding Ding Dong (͠≖ ͜ʖ͠≖)👌"
                 button.classList.add("ready");
                 button.addEventListener("click", () => {
                     animInterval = setInterval(preloaderAnim, 10);
